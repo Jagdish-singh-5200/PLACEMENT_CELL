687aee8a4f69c6a5af91431b6e97e49b2345cf49
Jagdish-singh-5200 <singhjagdish5200@gmail.com>
2023-07-01 21:17:05 +0530

    first commit

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(
       maxAge: 1000 * 60 * 100,
     },
     store: MongoStore.create({
-      mongoUrl: "mongodb+srv://singhjagdish5200:Jiggy5200@cluster0.jn4x85w.mongodb.net/placement?retryWrites=true&w=majority",
+      mongoUrl: MONGODB_URL,
       autoRemove: "disabled",
     }),
     function(err) {
